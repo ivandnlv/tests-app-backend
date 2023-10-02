4b97d7cee6f19f4e7eae9783c9934e076c705461
ivandnlv <daniloffivan0@gmail.com>
2023-10-02 22:22:15 +0500

    fix/all important data was transefered to .env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,20 @@
 import mysql from 'mysql';
-import { LOGIN, PASSWORD } from '../index';
+import {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_LOGIN,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} from '../utils/variables';
+
+// // Variables from .env file, need to create
 
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: '2281',
-  database: 'tests_db',
+  host: DATABASE_HOST,
+  port: Number(DATABASE_PORT),
+  user: DATABASE_LOGIN,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
 });
-
-connection.connect();
 
 export default connection;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import { config } from 'dotenv';
+import { PORT } from './utils/variables';
 import { testRouter, questionsRouter } from './routes';
-
-config();
-
-const PORT = process.env.PORT || 8080;
-export const LOGIN = process.env.DATABASE_LOGIN;
-export const PASSWORD = process.env.DATABASE_PASSWORD;
 
 const app = express();
 app.use(cors());
